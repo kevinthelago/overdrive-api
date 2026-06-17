cd7108f74f35e9bfd0f4656c356583fa6a34edc4
Kevin Lago <kevinthelago@gmail.com>
2026-06-17 14:28:17 -0400

    Revert "feat(scenario-analytics-api): implement SCEN-API and ANALYTICS-API"
    This reverts commit a914662b4dc55c35be8cbc4277f9a343b91d0efe.

--- src/main/kotlin/com/overdrive/scenario/domain/ScenarioRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.overdrive.scenario.domain
-
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.UUID
-
-interface ScenarioRepository : JpaRepository<Scenario, UUID> {
-    fun existsByName(name: String): Boolean
-    fun existsByNameAndIdNot(name: String, id: UUID): Boolean
-}
